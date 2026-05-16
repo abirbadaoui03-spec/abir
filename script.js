47fb7ec238010921152155b0540f21ee95506631
abirbd14 <abirbadaoui14@gmail.com>
2026-05-16 11:15:27 +0100

    ajouter

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("JavaScript fonctionne !");
+
+const inputname = document.getElementById("name");
+const inputemail = document.getElementById("email");
+const inputpassword = document.getElementById("password");
+const inputage = document.getElementById("age");
+const submitbtn = document.getElementById("submitbtn");
+
+console.log(inputname);
+console.log(inputemail);
+console.log(inputpassword);
+console.log(inputage);
+console.log(submitbtn);
+
+
+submitbtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Le bouton a été cliqué !");
+    console.log("Name:" + inputname.value);
+    console.log("Email:" + inputemail.value);
+    console.log("Email :" + inputemail.value);
+    console.log("Password :" + inputpassword.value);
+    console.log("Age :" + inputage.value);
+
+    alert("Name :" + inputname.value + "\nEmail:" + inputemail.value + "\nPassword :" + inputpassword.value + "\nAge :" + inputage.value);
+});
